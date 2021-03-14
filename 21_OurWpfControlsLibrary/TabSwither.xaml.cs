4107ea7303af5773e35cd7d19a0eab9a400a4018
amele.pilot <amele.pilot@gmail.com>
2021-03-14 14:47:37 +0300

    Удалил лишние недодедки проектов... сегодня принимаюсь к лессон2 работа с библиотекой

--- 21_OurWpfControlsLibrary/TabSwither.xaml.cs
+++ 21_OurWpfControlsLibrary/TabSwither.xaml.cs
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MailerMVVM
+namespace _21_OurWpfControlsLibrary
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для TabSwither.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class TabSwither : UserControl
     {
-        public MainWindow()
+        public TabSwither()
         {
             InitializeComponent();
         }
